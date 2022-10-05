1fcc520d0a78a5f72ac99d4af88eea0563338425
Daniil Galitskii <galitskii.daniil.webdev@gmail.com>
2022-10-05 05:05:04 +0200

    added collision, need to fix blocks render

--- cLearn/Source.c
+++ cLearn/Source.c
@@ -7,6 +7,7 @@
 typedef struct
 {
 	int x, y;
+	float dy;
 	short life;
 	char *name;
 } Man;
@@ -29,6 +30,43 @@ typedef struct
 	SDL_Texture *bridge;
 	SDL_Renderer *renderer;
 } GameState;
+
+void collisionDetect(GameState *game)
+{
+	for (int i = 0; i < 100; i++)
+	{
+		float mw = 64, mh = 64;
+		float mx = game->man.x, my = game->man.y;
+		float bx = game->ledges[i].x, by = game->ledges[i].y, bw = game->ledges[i].w, bh = game->ledges[i].h;
+
+		if (my + mh > by && my < by + bh)
+		{
+			if (mx < bx+bw && mx+mw > bx+bw)
+			{
+				game->man.x = bx + bw;
+				mx = bx + bw;
+			}
+			else if(mx+mw > bx && mx < bx)
+			{
+				game->man.x = bx - mw;
+				mx = bx - mw;
+			}
+		}
+
+		if (mx + mw > bx && mx < bx + bw)
+		{
+			if (my < by + bh && my > by) {
+				game->man.y = by + bh;
+				game->man.dy = 0;
+			}
+			else if (my+mh > by && my < by)
+			{
+				game->man.y = by - mh;
+				game->man.dy = 0;
+			}
+		}
+	}
+}
 
 void loadGame(GameState* game) 
 {
@@ -64,6 +102,66 @@ void loadGame(GameState* game)
 	game->manFrames[0] = SDL_CreateTextureFromSurface(game->renderer, surface);
 	SDL_FreeSurface(surface);
 
+	surface = IMG_Load("./images/run-00.png");
+	if (surface == NULL)
+	{
+		printf("Cannot find run-00.png \n\n");
+		SDL_Quit();
+		exit(1);
+	}
+	game->manFrames[1] = SDL_CreateTextureFromSurface(game->renderer, surface);
+	SDL_FreeSurface(surface);
+
+	surface = IMG_Load("./images/run-01.png");
+	if (surface == NULL)
+	{
+		printf("Cannot find run-01.png \n\n");
+		SDL_Quit();
+		exit(1);
+	}
+	game->manFrames[2] = SDL_CreateTextureFromSurface(game->renderer, surface);
+	SDL_FreeSurface(surface);
+
+	surface = IMG_Load("./images/run-02.png");
+	if (surface == NULL)
+	{
+		printf("Cannot find run-02.png \n\n");
+		SDL_Quit();
+		exit(1);
+	}
+	game->manFrames[3] = SDL_CreateTextureFromSurface(game->renderer, surface);
+	SDL_FreeSurface(surface);
+
+	surface = IMG_Load("./images/run-03.png");
+	if (surface == NULL)
+	{
+		printf("Cannot find run-03.png \n\n");
+		SDL_Quit();
+		exit(1);
+	}
+	game->manFrames[4] = SDL_CreateTextureFromSurface(game->renderer, surface);
+	SDL_FreeSurface(surface);
+
+	surface = IMG_Load("./images/run-04.png");
+	if (surface == NULL)
+	{
+		printf("Cannot find run-04.png \n\n");
+		SDL_Quit();
+		exit(1);
+	}
+	game->manFrames[5] = SDL_CreateTextureFromSurface(game->renderer, surface);
+	SDL_FreeSurface(surface);
+
+	surface = IMG_Load("./images/run-05.png");
+	if (surface == NULL)
+	{
+		printf("Cannot find run-05.png \n\n");
+		SDL_Quit();
+		exit(1);
+	}
+	game->manFrames[6] = SDL_CreateTextureFromSurface(game->renderer, surface);
+	SDL_FreeSurface(surface);
+
 	game->man.x = 320 - 40;
 	game->man.y = 240 - 40;
 
@@ -79,6 +177,8 @@ void loadGame(GameState* game)
 		game->ledges[i].x = i*256;
 		game->ledges[i].y = 400;
 	}
+	game->ledges[99].x = 350;
+	game->ledges[99].y = 200;
 }
 
 int processEvent(SDL_Window *window, GameState *game)
@@ -176,6 +276,8 @@ int main()
 	{
 		done = processEvent(window, &gameState);
 		
+		collisionDetect(&gameState);
+
 		doRender(renderer, &gameState);
 	}
 
